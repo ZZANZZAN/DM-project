fe8b0d659f1ac22186da2ad7816ba34833a7420c
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 23:45:41 +0300

    Update 1.h

--- modules/n/1.h
+++ modules/n/1.h
@@ -1,38 +1,25 @@
 #include <stdint.h>
-
-
 int N1(natural *A, natural *B) 
 {
-    
-    /* Compare length */
-    
+    /* Compare length */ 
     if((A -> length) > (B -> length)) 
-    {
-        
-        return 2;
-        
+    {  
+        return 2;   
     } 
     else if((A -> length) < (B -> length)) 
-    {
-        
-        return 1;
-        
+    {  
+        return 1;  
     } 
     else 
-    {
-        
+    {  
         /* Digits comparison */
-        
         for(size_t i = (A -> length) - 1; i != SIZE_MAX; --i) 
-        {
-            
+        { 
             if(A -> digits[i] > (B -> digits[i]))
-                return 2;
-                
+                return 2;   
             else if((A -> digits[i]) < (B -> digits[i]))
                 return 1;
         }
-        
         return 0;
     }
 }
