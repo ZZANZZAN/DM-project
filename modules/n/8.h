520a08593d9be41c5b48e51d352dad12b20aafe1
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 23:55:46 +0300

    Add files via upload

---
+++ modules/n/8.h
@@ -0,0 +1,29 @@
+natural *N8(natural *A, natural *B)
+{
+    natural *res;
+    natural *temp;
+    size_t i , temp_length;
+    res = init_natural(1);
+    res -> digits[0] = 0;
+    if (A -> length > B -> length)
+    {
+        temp_length = B -> length;
+        for (i = 0 ; i < temp_length; i++)
+        {
+            temp = N6 (A, B -> digits[i]);
+            temp = N7 (temp , i);
+            res = N4 (temp , res);
+        }
+    }
+    else
+    {
+        temp_length = A -> length;
+        for (i = 0 ; i < temp_length ; i++)
+        {
+        	temp = N6 (B , A -> digits[i]);
+            temp = N7 (temp , i);
+        	res = N4 (temp , res);
+        }
+    }
+    return res;
+}
