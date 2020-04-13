3d4f3860f451e2b5186601604cc15bf85854a019
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 23:44:49 +0300

    Update 2.h

--- modules/n/2.h
+++ modules/n/2.h
@@ -1,12 +1,9 @@
 //проверка на ноль, проход по масссиву в поисках отличной от нуля цифры
 bool N2(natural *x) 
-{
-    
+{  
     bool flag = false;
-
     for (size_t i = 0; i < (x -> length); i++)
         if (x -> digits[i] != 0)
-            flag = true;
-    
+            flag = true;   
     return flag;
 }
