47027d2587ad3e63100b9c3cab897990fab255bf
Sonnennn <62358129+Sonnennn@users.noreply.github.com>
2020-04-13 15:51:41 +0300

    Create 3.h

---
+++ modules/z/3.h
@@ -0,0 +1,17 @@
+
+integer *Z3(integer *A){
+
+    //checking the number at 0
+
+    if (A->digits[0]==0)
+        return(A);
+
+    //multiplying a number by (-1)
+    if (A->sign==true)
+
+            A->sign=false;
+
+    else A->sign=true;
+
+    return(A);
+}
