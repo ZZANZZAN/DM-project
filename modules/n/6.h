5a542a0511d085948b4bdd8a8d3a97b9dc7287d4
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-13 22:42:32 +0300

    Create 6.h

---
+++ modules/n/6.h
@@ -0,0 +1,32 @@
+natural *N6(natural *A, int b) {
+	natural *C;
+    C = init_natural(A -> length);
+    for(int i = 0 ; i < (A -> length) ; i++) C -> digits[i] = (A -> digits[i]);
+    
+	//checking for zero
+	if(b){
+		
+	    size_t i = 0;
+        int r = 0, c = 0;
+	    //the main loop of the multiplication
+	    while(i != (A -> length)){
+            c = (C -> digits[i]) * b;
+            if(r) c += r;   
+            C -> digits[i] = c % 10;
+            r = c / 10;
+			i++;    
+        }
+        //case with a new category
+        if(r){
+            resize_natural(C, (C -> length) + 1);
+            C -> digits[(C -> length) - 1] = r; 
+        }
+        
+    }else{
+    	//zeroing
+    	C -> length = 1;
+    	C -> digits[0] = 0;
+	}
+    
+	return C;
+}
