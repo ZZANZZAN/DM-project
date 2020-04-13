68963384e3380e86ed4be1f62bab7c316a2679d2
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-13 23:07:43 +0300

    Update 5.h

--- modules/n/5.h
+++ modules/n/5.h
@@ -1,43 +1,50 @@
 // Nikolaenko and Kacer
 natural *N5(natural *A, natural *B) {
+	natural *C, *D;
     int q;
-    // compare the numbers
+    C = init_natural(A -> length);
+    D = init_natural(B -> length);
+    C -> length = A -> length;
+    D -> length = B -> length;
+    for(int i = 0; i < (C -> length); i++) C -> digits[i] = (A -> digits[i]);
+    for(int i = 0; i < (D -> length); i++) D -> digits[i] = (B -> digits[i]);
+	// compare the numbers
     q = N1(A,B);
 
     if(q == 2){
-        for(int i = 0; i < (B -> length); i++){
-            if((A -> digits[i]) >= (B -> digits[i])) A -> digits[i] -= (B -> digits[i]); // subtraction
+        for(int i = 0; i < (D -> length); i++){
+            if((C -> digits[i]) >= (D -> digits[i])) C -> digits[i] -= (D -> digits[i]); // subtraction
             else{ // take a ten
                 int k = i + 1;
-                while((A -> digits[k]) == 0) k++;
+                while((C -> digits[k]) == 0) k++;
                 
-                A -> digits[k--] -= 1; // subtract one from whom we borrow
-                while(k != i) A -> digits[k--] = 9; // if there are zeros between who is being borrowed and who is being borrowed for, then we replace them with nines
+                C -> digits[k--] -= 1; // subtract one from whom we borrow
+                while(k != i) C -> digits[k--] = 9; // if there are zeros between who is being borrowed and who is being borrowed for, then we replace them with nines
                 
-                A -> digits[i] += (10 - (B -> digits[i])); 
+                C -> digits[i] += (10 - (D -> digits[i])); 
             } 
         }
-        if(A -> digits[(A -> length) - 1] == 0) resize_natural(A, (A -> length) - 1);
+        if(C -> digits[(C -> length) - 1] == 0) resize_natural(C, (C -> length) - 1);
     }else if(q == 1){
-	    for(int i = 0; i < (A -> length); i++){
-            if((B -> digits[i]) >= (A -> digits[i])) B -> digits[i] -= (A -> digits[i]); // subtraction
+	    for(int i = 0; i < (C -> length); i++){
+            if((D -> digits[i]) >= (C -> digits[i])) D -> digits[i] -= (C -> digits[i]); // subtraction
             else{ // take a ten
                 int k = i + 1;
-                while((B -> digits[k]) == 0) k++;
+                while((D -> digits[k]) == 0) k++;
                 
-                B -> digits[k--] -= 1; // subtract one from whom we borrow
-                while(k != i) B -> digits[k--] = 9; // if there are zeros between who is being borrowed and who is being borrowed for, then we replace them with nines
+                D -> digits[k--] -= 1; // subtract one from whom we borrow
+                while(k != i) D -> digits[k--] = 9; // if there are zeros between who is being borrowed and who is being borrowed for, then we replace them with nines
                 
-                B -> digits[i] += (10 - (A -> digits[i])); 
+                D -> digits[i] += (10 - (C -> digits[i])); 
             } 
         } 
-        if(B -> digits[(B -> length) - 1] == 0) resize_natural(B, (B -> length) - 1);
+        if(D -> digits[(D -> length) - 1] == 0) resize_natural(D, (D -> length) - 1);
     }else if(q == 0){ // when the numbers are equal
-    	A -> digits[0] = 0;
-    	resize_natural(A, 1);
-    	A -> length = 1;
+    	C -> digits[0] = 0;
+    	resize_natural(C, 1);
+    	C -> length = 1;
 	}
     
-    if((q == 2) || (q == 0)) return A;
-    else return B;
+    if((q == 2) || (q == 0)) return C;
+    else return D;
 }
