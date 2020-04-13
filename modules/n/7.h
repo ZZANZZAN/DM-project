330c07425d770ba550865afc4aef7c8737f268f6
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 23:50:14 +0300

    Update 7.h

--- modules/n/7.h
+++ modules/n/7.h
@@ -1,12 +1,9 @@
-#include <stdint.h>		// SIZE_MAX
-
-
+#include <stdint.h>
 natural *N7(natural *A, int k)
 {
-	if(N2(A) == false) return A;
-	
+	if(N2(A) == false) 
+		return A;
     resize_natural(A, (A -> length) + k);// увеличение размера массива, на k элементов
-    
     for(size_t i = A -> length - 1; i >= (size_t)k && i != SIZE_MAX; --i)// заполнение новых ячеек массива 0
     {
         A -> digits[i] = A -> digits[i - k];
@@ -14,6 +11,5 @@ natural *N7(natural *A, int k)
     
     for(size_t i = 0; i < (size_t)k; ++i)
 		A -> digits[i] = 0;
-    
     return A;
 }
