22e62018796d16d9b1f21f315bbbe68f18c781d9
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-14 00:00:07 +0300

    Update 3.h

--- modules/z/3.h
+++ modules/z/3.h
@@ -1,17 +1,13 @@
 
-integer *Z3(integer *A){
-
+integer *Z3(integer *A)
+{
     //checking the number at 0
-
-    if (A->digits[0]==0)
+    if (A -> digits[0] == 0)
         return(A);
-
     //multiplying a number by (-1)
-    if (A->sign==true)
-
-            A->sign=false;
-
-    else A->sign=true;
-
+    if (A -> sign == true)
+            A -> sign = false;
+    else 
+        A -> sign = true;
     return(A);
 }
