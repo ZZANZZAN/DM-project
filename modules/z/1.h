c59ebe65f1f2d8f83a24327edc6bfe78a1b83fba
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 23:56:53 +0300

    Update 1.h

--- modules/z/1.h
+++ modules/z/1.h
@@ -3,6 +3,7 @@ natural *Z1(integer *A)
     size_t i;
     natural *B;
     B = init_natural(A -> length);
-    for(i = 0 ; i < B-> length ; i++) B -> digits[i] = A -> digits[i];
+    for(i = 0 ; i < B-> length ; i++)
+        B -> digits[i] = A -> digits[i];
     return B;
 }
