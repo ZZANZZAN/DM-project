4a72613869702eef77879df6388f047ffea23e40
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-14 00:01:18 +0300

    Update 4.h

--- modules/z/4.h
+++ modules/z/4.h
@@ -1,10 +1,10 @@
 integer *Z4(natural *A)
 {
-size_t i;
-integer *B;
-B = init_integer(A -> length);
-for (i = 0 ; i < A -> length ; i++) B->digits[i] = A->digits[i];
-B -> sign = true;
-return B;
-
+    size_t i;
+    integer *B;
+    B = init_integer(A -> length);
+    for (i = 0 ; i < A -> length ; i++) 
+        B -> digits[i] = A -> digits[i];
+    B -> sign = true;
+    return B;
 }
