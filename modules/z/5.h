3f30e2e4cf90ca6644b8bdaf07b5806645116bd8
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 17:44:44 +0300

    Add files via upload
    Added modules Z1 and Z5

---
+++ modules/z/5.h
@@ -0,0 +1,8 @@
+natural *Z5(integer *A)
+{
+    size_t i;
+    natural *B;
+    B = init_natural(A -> length);
+    for(i = 0 ; i < B-> length ; i++) B -> digits[i] = A -> digits[i];
+    return B;
+}
