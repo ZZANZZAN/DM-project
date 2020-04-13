16c2f788c46ac153da7493c4efe3b425f9633bce
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-13 23:42:13 +0300

    Add files via upload

---
+++ modules/n/3.h
@@ -0,0 +1,21 @@
+natural *N3(natural *A)
+{
+size_t i;
+    natural *res;
+	res = init_natural(A -> length);
+	for(i = 0 ; i < res -> length ; i++)
+        res -> digits[i]  = A -> digits[i];
+	/* Skipping nines */
+	i = 0;
+	while(i < (res -> length) && (res -> digits[i]) == 9)
+		res -> digits[i++] = 0;
+	if(i == (res -> length))
+	{
+		/* Increasing number length */
+		resize_natural(res, (res -> length) + 1);
+		res -> digits[(res -> length) - 1] = 1;
+	}
+	else
+        ++(res -> digits[i]);
+	return res;
+}
