ae4c19be4649e5f49ade5faf4fe4b8dc10e9dd62
onekitaev <60344777+onekitaev@users.noreply.github.com>
2020-04-16 00:10:37 +0300

    Update 6.h

--- modules/z/6.h
+++ modules/z/6.h
@@ -1,29 +1,26 @@
 integer *Z6(integer *A, integer *B)
 {
-    int k1, k2;
     integer *R;
     natural *aN, *bN, *L;
-    k1 = Z2(A);
-    k2 = Z2(B);
     aN = Z1(A);
     bN = Z1(B);
     R = init_integer(aN -> length);
     R = init_integer(bN -> length);
     R = NULL;
 
-    if ((k1 == 2) && (k2 == 2)) {
+    if ((Z2(A) == 2) && (Z2(B) == 2)) {
         L = N4(aN, bN);
         R = init_integer(L -> length);
         R = Z4(L);
         R -> sign = true;
     }
-    else if ((k1 == 1) && (k2 == 1)) {
+    else if ((Z2(A) == 1) && (Z2(B) == 1)) {
         L = N4(aN, bN);
         R = init_integer(L -> length);
         R = Z4(L);
         R -> sign = false;
     }
-    else if ((k1 == 2) && (k2 == 1)) {
+    else if ((Z2(A) == 2) && (Z2(B) == 1)) {
         if ((N1(aN, bN) == 2) || (N1(aN, bN) == 0)) {
             R = Z4(N5(aN, bN));
             R -> sign = true;
@@ -33,7 +30,7 @@ integer *Z6(integer *A, integer *B)
             R -> sign = false;
         }
     }
-    else if ((k1 == 1) && (k2 == 2)) {
+    else if ((Z2(A) == 1) && (Z2(B) == 2)) {
         if (N1(aN, bN) == 2) {
             R = Z4(N5(aN, bN));
             R -> sign = false;
@@ -43,21 +40,21 @@ integer *Z6(integer *A, integer *B)
             R -> sign = true;
         }
     }
-    else if (k1 == 0) {
-        if (k2 == 2) {
+    else if (Z2(A) == 0) {
+        if (Z2(B) == 2) {
             B -> sign = true;
         }
-        else if (k2 == 1) {
+        else if (Z2(B) == 1) {
             B -> sign = false;
         }
         R = init_integer(B -> length);
         R = B;
     }
-    else if (k2 == 0) {
-        if (k1 == 2) {
+    else if (Z2(B) == 0) {
+        if (Z2(A) == 2) {
             A -> sign = true;
         }
-        else if (k1 == 1) {
+        else if (Z2(A) == 1) {
             A -> sign = false;
         }
         R = init_integer(A -> length);
