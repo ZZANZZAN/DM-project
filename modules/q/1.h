acb41e996ebc7080b79f743c0e83773cfa2f28c6
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2020-04-18 23:15:11 +0300

    Update 1.h

--- modules/q/1.h
+++ modules/q/1.h
@@ -1,7 +1,5 @@
 fraction *Q1(fraction *A) {
     fraction *res = copy_fraction(A);
-    if (A -> numerator -> sign == true){res -> numerator -> sign = true;}
-    else{res -> numerator -> sign = false;}
     res -> numerator = Z9(A -> numerator, Z4(N13(A -> denominator, Z1(A -> numerator))));
     res -> denominator = N11(A -> denominator, N13(A -> denominator, Z1(A -> numerator)));
     return res;
