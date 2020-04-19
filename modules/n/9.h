a8dc40ad9a2932d8f934d3363674643f243f596e
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-20 00:30:17 +0300

    Update 9.h

--- modules/n/9.h
+++ modules/n/9.h
@@ -1,4 +1,4 @@
-// Nikolaenko
+// 9305_Nikolaenko
 
 natural *N9(natural *A, natural *B, int c) {
     natural *C, *D, *R;
@@ -9,17 +9,19 @@ natural *N9(natural *A, natural *B, int c) {
     
           // compare
         int q = N1(A, B);
+        
           // multiplication and subtraction      
         if((q == 2) || (q == 0)) 
         {
             D = N6(D, c);
             C = N5(C, D);
+            
             R = init_natural(C -> length);
+            
             for(i = 0; i < (C -> length); i++) 
                 R -> digits[i] = (C -> digits[i]);
-        }else if(q == 1){
-        	R = copy_natural(A);
-		} 
+        }
+		else if(q == 1) R = copy_natural(A); 
         
     return R;
 }
