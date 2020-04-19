b2dd62c77bd596e725320dc1d1a2fe6250235780
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-19 23:55:17 +0300

    Create 11.h

---
+++ modules/n/11.h
@@ -0,0 +1,66 @@
+// Nikolaenko
+
+natural *N11(natural *A, natural *B) {
+    natural *C, *D, *N, *R;
+    size_t i, c = 0;
+    signed char t = 0;
+    size_t u;
+    
+	N = init_natural(100);
+	R = init_natural(100);
+    C = copy_natural(A);
+    D = copy_natural(B);
+         
+            size_t j, b;
+        	u = C -> length;
+       	
+        	for(j = (D -> length); j >= 1; j--) N -> digits[j - 1] = (C -> digits[--u]);
+        	
+        	N -> length = D -> length;      
+            C -> length -= (D -> length);
+            	
+	 		if(N1(N, D) == 1)
+			{
+				N -> length += 1;
+				for(b = (N -> length) - 1; b >= 1; b--) N -> digits[b] = (N -> digits[b - 1]);
+			    N -> digits[0] = (C -> digits[--u]);
+				C -> length -= 1;	
+		    } 
+	 		
+			i = (C -> length);
+			
+			t = N10(N, D);
+			R -> digits[c++] = t;
+			R -> length = 1;
+			
+			N = N9(N, D, t);
+			if((N -> length == 1) && (N -> digits[0] == 0)) N -> length = 0;
+	
+        	while(i--)
+			{
+	            N -> length += 1;
+	            
+				for(b = (N -> length); b >= 2; b--) N -> digits[b - 1] = (N -> digits[b - 2]);
+        	    
+				N -> digits[0] = (C -> digits[u - 1]);
+                if(u != 0) u--;
+        	    
+				t = N10(N, D);
+        	    R -> digits[c++] = t;            			
+			
+				N = N9(N, D, t);
+			    if((N -> length == 1) && (N -> digits[0] == 0)) N -> length = 0;
+			    
+				C -> length -= 1;
+		    }
+		    
+		    R -> length = c;
+		    
+		    for(b = 0; b < c/2; b++){
+		    	t = R -> digits[b];
+		    	R -> digits[b] = R -> digits[c - b - 1];
+		    	R -> digits[c - b - 1] = t;
+			}
+        
+    return R;
+}
