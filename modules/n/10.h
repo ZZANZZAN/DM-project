6c0a391e31714b2721075b7d3f8fef2caba3bf3b
NikolaenkoKonstantin <62340704+NikolaenkoKonstantin@users.noreply.github.com>
2020-04-20 00:29:54 +0300

    Update 10.h

--- modules/n/10.h
+++ modules/n/10.h
@@ -1,4 +1,4 @@
-// Nikolaenko
+// 9305_Nikolaenko
 
 signed char N10(natural *A, natural *B) {
     natural *C, *D, *N;
@@ -16,26 +16,25 @@ signed char N10(natural *A, natural *B) {
         {
             N = init_natural(B -> length);
             N = init_natural(B -> length);
-            for(i = 0; i < (N -> length); i++) 
+           
+		    for(i = 0; i < (N -> length); i++) 
 	            N -> digits[i] = (B -> digits[i]);
             
             D = N7(D, 1);
             
-                while((N1(C, D) == 2) || (N1(C, D) == 0)) {
-                    D = N7(D, 1);
-                    N = N7(N, 1);
-                }
+            while((N1(C, D) == 2) || (N1(C, D) == 0)) 
+			{
+                D = N7(D, 1);
+                N = N7(N, 1);
+            }
             
-            while((N1(C, N) == 2) || (N1(C, N) == 0)){
+            while((N1(C, N) == 2) || (N1(C, N) == 0))
+			{
                 C = N5(C, N);
                 t++;
             }
-            
         }
-        else if(q == 1) 
-        {    
-            t = 0;
-        }    
+        else if(q == 1) t = 0;    
         else if(q == 0) t = 1;
         
     return t;
