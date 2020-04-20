061bb54973005c4ebb4ec30be4e05b81d686c15b
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2020-04-20 14:25:41 +0300

    Update 1.h

--- modules/q/1.h
+++ modules/q/1.h
@@ -1,9 +1,9 @@
 fraction *Q1(fraction *A) {
     fraction *res = copy_fraction(A);
-    natural *B = init_natural(1);
-    B -> digits[0] = 1;
+    natural *B = NULL;
     B = N13(A -> denominator, Z1(A -> numerator));
     res -> numerator = Z9(A -> numerator, Z4(B));
     res -> denominator = N11(A -> denominator, B);
+    free(B);
     return res;
 }
