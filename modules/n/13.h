e85b16b7826625332cea1465207a8be5636ba335
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-20 12:29:53 +0300

    Add files via upload

---
+++ modules/n/13.h
@@ -0,0 +1,27 @@
+natural *N13(natural *A,natural *B)
+{
+    natural *first = NULL, *second = NULL;
+    first = copy_natural(A);
+    second = copy_natural(B);
+    while(N2(first) && N2(second))
+    {
+        if(N1(first , second)==2)
+        {
+            first = N12(first , second);
+        }
+        else
+        {
+           second = N12(second , first);
+        }
+    }
+    if(N2(first))
+    {
+        free(second);
+        return first;
+    }
+    else
+    {
+        free(first);
+        return second;
+    }
+}
---
+++ modules/n/14.h
@@ -0,0 +1,10 @@
+natural *N14(natural *A,natural *B)
+{
+    natural *first = NULL, *second = NULL, *res = NULL;
+    first = copy_natural(A);
+    second = copy_natural(B);
+    res = N11(N8(first, second), N13(first, second)); //MUL_NN_N - Умножение нат. чисел, GCF_NN_N - их НОД
+    free(first);
+    free(second);
+    return res;
+}
