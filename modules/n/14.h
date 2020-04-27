24141be8dbf2f6f9c55144fbb6ccf62b37e8ac7c
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 19:54:11 +0300

    Update 14.h

--- modules/n/14.h
+++ modules/n/14.h
@@ -1,9 +1,13 @@
 natural *N14(natural *A,natural *B)
 {
-    natural *first = NULL, *second = NULL, *res = NULL;
-    first = copy_natural(A);
-    second = copy_natural(B);
-    res = N11(N8(first, second), N13(first, second)); //MUL_NN_N - Умножение нат. чисел, GCF_NN_N - их НОД
+    natural *first = NULL, *second = NULL, *res = NULL, *temp1 = NULL, *temp2 = NULL;
+    first = copy_natural (A);
+    second = copy_natural (B);
+    temp1 = N8 (first, second);
+    temp2 = N13 (first, second);
+    res = N11 (temp1, temp2);
+    free_natural(temp1);
+    free_natural(temp2);
     free_natural(first);
     free_natural(second);
     return res;
