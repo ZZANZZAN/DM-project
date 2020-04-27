ec71de0823f0987eeac21c6843eaafd7435ebc01
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 20:10:17 +0300

    Update 4.h

--- modules/q/4.h
+++ modules/q/4.h
@@ -1,6 +1,5 @@
-//Boykova V.A.
 integer *Q4( fraction *A) {
-        integer *R;
+        integer *R=NULL;
         //test for the ability to transform into integer
         if ((( A -> denominator -> length ) == 1) && (( A -> denominator -> digits[0] ) == 1)){
             R = copy_integer(A -> numerator);
