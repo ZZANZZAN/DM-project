fb507c8fcd5d552a5ec58fdadb05c6c6243378ae
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 19:46:37 +0300

    Update 3.h

--- modules/n/3.h
+++ modules/n/3.h
@@ -2,23 +2,16 @@ natural *N3(natural *A)
 {
     natural *res;
 	res = copy_natural(A);
-	/* Skipping nines */
-
 	size_t i = 0;
-
 	while(i < (res -> length) && (res -> digits[i]) == 9)
 		res -> digits[i++] = 0;
-
-
+    // Increasing number length
 	if(i == (res -> length))
 	{
-		/* Increasing number length */
-
 		resize_natural(res, (res -> length) + 1);
 		res -> digits[(res -> length) - 1] = 1;
-
-	} else ++(res -> digits[i]);
-
-
+	}
+	else
+        ++(res -> digits[i]);
 	return res;
 }
