4d2159ea8971e1dd20354f04bcf5d715f636310d
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 22:15:09 +0300

    Update 3.h

--- modules/z/3.h
+++ modules/z/3.h
@@ -1,17 +1,18 @@
 integer *Z3(integer *A)
 {
     integer *res;
-    res = init_integer(A -> length);
-    res -> sign = A -> sign;
-    for(size_t i = 0 ; i < res -> length ; i++)
-        res -> digits[i] = A -> digits[i];
-    //checking the number at 0
-    if ((res -> digits[0] == 0)&&(res -> length == 1))
-        return(res);
-    //multiplying a number by (-1)
-    if (res -> sign == true)
-            res -> sign = false;
+
+    res = copy_integer(A);
+
+    if ((res->digits[0] == 0)&&(res -> length == 1))
+        res->sign=true;
     else
-        res -> sign = true;
-    return(res);
+    {
+        if (res->sign)
+            res->sign=false;
+        else
+            res->sign=true;
+    }
+
+    return res;
 }
