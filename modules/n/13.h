841e19898c88bbdf9c99342d2b6d8f433e7c885c
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 19:53:24 +0300

    Update 13.h

--- modules/n/13.h
+++ modules/n/13.h
@@ -1,17 +1,23 @@
 natural *N13(natural *A,natural *B)
 {
-    natural *first = NULL, *second = NULL;
+    natural *first = NULL, *second = NULL, *temp = NULL;
     first = copy_natural(A);
     second = copy_natural(B);
     while(N2(first) && N2(second))
     {
         if(N1(first , second)==2)
         {
-            first = N12(first , second);
+            temp = N12(first , second);
+            free_natural(first);
+            first = temp;
+            free_natural(temp);
         }
         else
         {
-           second = N12(second , first);
+            temp = N12(second , first);
+            free_natural(second);
+            second = temp;
+            free_natural(temp);
         }
     }
     if(N2(first))
