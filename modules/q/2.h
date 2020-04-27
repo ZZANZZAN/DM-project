90aee775f08ad3eb2205cf867acb2c8a94b9f90a
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 20:09:04 +0300

    Update 2.h

--- modules/q/2.h
+++ modules/q/2.h
@@ -1,28 +1,34 @@
-// 9305_Nikolaenko
 #include <stdbool.h>
 
 bool Q2(fraction *A) {
-    
-    if(((A -> denominator -> length) == 1) && (A -> denominator -> digits[0] == 1)) 
-	{
-		return true;
-	}
-    else
-    {
-    	natural *B, *C;
-    	
-        B = init_natural(A -> numerator -> length);
-        for(size_t j = 0; j < (B -> length); j++) B -> digits[j] = (A -> numerator -> digits[j]);
-        C = copy_natural(A -> denominator);
-    
-        if(N1(B, C) == 0) return true;
-        else
-		{    	
-			natural *F;
-			F = N13(B, C);
-			
-			if(N1(F, C) == 0) return true;
-			else return false;
-		} 
+
+    if (((A->denominator->length) == 1) && (A->denominator->digits[0] == 1)) {
+        return true;
+    } else {
+        natural *B = NULL, *C = NULL;
+
+        B = init_natural(A->numerator->length);
+        for (size_t j = 0; j < (B->length); j++) B->digits[j] = (A->numerator->digits[j]);
+        C = copy_natural(A->denominator);
+
+        if (N1(B, C) == 0) {
+            free_natural(B);
+            free_natural(C);
+            return true;
+        } else {
+            natural *F = NULL;
+            F = N13(B, C);
+
+            if (N1(F, C) == 0) {
+                free_natural(B);
+                free_natural(C);
+                free_natural(F);
+                return true;
+            } else {
+                free_natural(B);
+                free_natural(C);
+                free_natural(F);
+                return false; }
+        }
     }
 }
