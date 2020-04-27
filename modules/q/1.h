1272fcba2a2ee4095d465d4a0edf4bfe3615c914
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 20:08:16 +0300

    Update 1.h

--- modules/q/1.h
+++ modules/q/1.h
@@ -1,9 +1,33 @@
-fraction *Q1(fraction *A) {
+fraction *Q1(fraction *A)
+{
     fraction *res = copy_fraction(A);
     natural *B = NULL;
-    B = N13(A -> denominator, Z1(A -> numerator));
-    res -> numerator = Z9(A -> numerator, Z4(B));
+    int sign = 1;
+    integer *temp1=NULL;
+    natural* temp_numerator = Z1(A -> numerator), * temp_denominator = res->denominator , *temp2 = NULL;
+    B = N13(A -> denominator, temp_numerator);
+
+    free_natural(temp_numerator);
+    temp2 = Z1(A->numerator);
+    sign = Z2(A->numerator);
+    temp1 = res->numerator;
+    temp_numerator = N11(temp2, B);
+
+    res->numerator=Z4(temp_numerator);
+
+    free_natural(temp_numerator);
+    free_integer(temp1);
+    free_natural(temp2);
+
     res -> denominator = N11(A -> denominator, B);
+
+    if (sign==1)
+    {
+        res->numerator->sign=false;
+
+    }
+
+    free_natural(temp_denominator);
     free_natural(B);
     return res;
 }
