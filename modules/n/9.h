2d69f89db7dc0fd05e9798c446ad340bad25691c
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-27 19:50:14 +0300

    Update 9.h

--- modules/n/9.h
+++ modules/n/9.h
@@ -1,27 +1,8 @@
-natural *N9(natural *A, natural *B, int c) {
-    natural *C, *D, *R;
-    size_t i;
-
-    C = copy_natural(A);
-    D = copy_natural(B);
-
-          // compare
-        int q = N1(A, B);
-
-          // multiplication and subtraction
-        if((q == 2) || (q == 0))
-        {
-            D = N6(D, c);
-            C = N5(C, D);
-
-            R = init_natural(C -> length);
-
-            for(i = 0; i < (C -> length); i++)
-                R -> digits[i] = (C -> digits[i]);
-        }
-		else R = copy_natural(A);
+natural *N9(natural *A, natural *B, int c)
+{
+    natural *C = NULL, *res = NULL;
+    C = N6 (B , c);
+    res = N5 (A , C);
     free_natural(C);
-    free_natural(D);
-    return R;
+    return res;
 }
-
