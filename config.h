5076cbfc1be4ccab58a2dd8d55b89bd699b0ff18
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-28 14:51:45 +0300

    Update config.h

--- config.h
+++ config.h
@@ -37,10 +37,10 @@
 
 // #define P_1
 // #define P_2
-// #define P_3
-// #define P_4
-// #define P_5
-// #define P_6
+#define P_3
+#define P_4
+#define P_5
+#define P_6
 // #define P_7
 // #define P_8
 // #define P_9
