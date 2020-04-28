fce8aab54852eb80258c2832c45ac54fad7c891b
Andrey <29797602+kogutenko@users.noreply.github.com>
2020-04-28 15:01:41 +0300

    P modules disabled

--- config.h
+++ config.h
@@ -36,11 +36,11 @@
 #define Q_8
 
 // #define P_1
-#define P_2
-#define P_3
-#define P_4
-#define P_5
-#define P_6
+// #define P_2
+// #define P_3
+// #define P_4
+// #define P_5
+// #define P_6
 // #define P_7
 // #define P_8
 // #define P_9
