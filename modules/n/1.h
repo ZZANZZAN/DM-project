4999bdb205f7e6775cb5ab259429bb81eda81f28
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 02:49:10 +0300

    Update 1.h

--- modules/n/1.h
+++ modules/n/1.h
@@ -1,8 +1,9 @@
 #include <stdint.h>
-
+// Боголюбова Вера 9305
+//Данный модуль сравнивает два натуральных числа
 int N1(natural *A, natural *B)
 {
-    /* Compare length */
+    //Сравнение длин чисел
     if((A -> length) > (B -> length))
     {
         return 2;
@@ -13,8 +14,8 @@ int N1(natural *A, natural *B)
     }
     else
     {
-        /* Digits comparison */
-        for(size_t i = (A -> length) - 1; i != SIZE_MAX; --i)
+        //Если числа одинаковой длины, то сравниваем их по цифрам
+        for(size_t i = (A -> length) - 1; i != SIZE_MAX; --i)//В size_t когда счётчик доходит до 0 и уменьшается на единицу, то он становится равен константе SIZE_MAX, определенной в stdint
         {
             if(A -> digits[i] > (B -> digits[i]))
                 return 2;
