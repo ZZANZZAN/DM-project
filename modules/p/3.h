5d4fa70bb0c4a5f79cfbfd6c4cfd62f60f67a5bd
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-28 22:50:32 +0300

    Update 3.h

--- modules/p/3.h
+++ modules/p/3.h
@@ -2,14 +2,21 @@ polynomial *P3(polynomial *A, fraction *B)
 {
 	polynomial *res = NULL;
 	fraction *temp = NULL, *temp1 = NULL;
+	
 	res = copy_polynomial(A);
-	temp = copy_fraction (B);
+	
+	temp = Q3 (B);
+	
 	for (size_t i = 0; i <= A -> degree; i++)
 	{
 		temp1 = Q7 (A -> factors[i], temp);
+	
 		res -> factors[i] = copy_fraction(temp1);
+	
 		free_fraction(temp1);
 	}
+	
 	free_fraction(temp);
+	
 	return res;
 }
