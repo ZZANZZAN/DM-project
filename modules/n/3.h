2420a2306d9fb23d339b7452b3346165554afe56
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 02:56:20 +0300

    Update 3.h

--- modules/n/3.h
+++ modules/n/3.h
@@ -1,17 +1,19 @@
+//Бойкова Валерия 9305
+//Данный модуль добавляет к числу единицу
 natural *N3(natural *A)
 {
-    natural *res;
+	natural *res;
 	res = copy_natural(A);
 	size_t i = 0;
-	while(i < (res -> length) && (res -> digits[i]) == 9)
+	while(i < (res -> length) && (res -> digits[i]) == 9)//Идём циклом с начала числа и заменяем 9 на 0
 		res -> digits[i++] = 0;
-    // Increasing number length
+	//Условие при переполнении числа
 	if(i == (res -> length))
 	{
 		resize_natural(res, (res -> length) + 1);
 		res -> digits[(res -> length) - 1] = 1;
 	}
 	else
-        ++(res -> digits[i]);
+        ++(res -> digits[i]);//Прибавление к цифре единицу
 	return res;
 }
