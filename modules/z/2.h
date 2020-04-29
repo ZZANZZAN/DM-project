4f235cd9979da55cce61ab21a03574e0b2d42bc4
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:34:21 +0300

    Update 2.h

--- modules/z/2.h
+++ modules/z/2.h
@@ -1,14 +1,13 @@
-
-int Z2(integer *A){
-
-    //checking the number at 0
-
-    if ((A->digits[0]==0)&&(A -> length == 1))
+//Есин Артём 9305
+//Определение положительности числа
+int Z2(integer *A)
+{
+    //Проверка на ноль
+    if ((A -> digits[0] == 0) && (A -> length == 1))
         return(0);
 
-    //checking the number by sign
-    if (A->sign==true)
-
+    //Проверка знака числа
+    if (A -> sign == true)
         return (2);
 
     else return (1);
