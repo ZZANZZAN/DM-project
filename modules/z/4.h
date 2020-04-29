a3be9a0cbb5ee2f791b0f35b6af3ff30d1d87fb2
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:37:52 +0300

    Update 4.h

--- modules/z/4.h
+++ modules/z/4.h
@@ -1,10 +1,12 @@
+//Игнатьев Константин 9305
+//Преобразование натурального в целое
 integer *Z4(natural *A)
 {
     size_t i;
     integer *B;
     B = init_integer(A -> length);
     for (i = 0 ; i < A -> length ; i++) 
-        B -> digits[i] = A -> digits[i];
+        B -> digits[i] = A -> digits[i];//Копирование цифр
     B -> sign = true;
     return B;
 }
