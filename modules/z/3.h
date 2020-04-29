1b53dc3cd5c46160a22475fa0c2761146c0d6db4
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:36:29 +0300

    Update 3.h

--- modules/z/3.h
+++ modules/z/3.h
@@ -1,14 +1,16 @@
+//Игнатьев Константин 9305
+//Умножение целого числа на -1
 integer *Z3(integer *A)
 {
     integer *res;
 
     res = copy_integer(A);
 
-    if ((res->digits[0] == 0)&&(res -> length == 1))
+    if ((res->digits[0] == 0)&&(res -> length == 1))//Проверка на ноль
         res->sign=true;
     else
     {
-        if (res->sign)
+        if (res->sign)//Изменение знака
             res->sign=false;
         else
             res->sign=true;
