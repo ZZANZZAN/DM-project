fb9df0a026f8024c898c0f81cd78350c0d28848f
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 04:12:59 +0300

    Update 8.h

--- modules/z/8.h
+++ modules/z/8.h
@@ -8,15 +8,28 @@ integer *Z8(integer *A, integer *B) {
 
     if((q) && (f))
     {
-        natural *C, *D;
-        C = Z1(A);
+        natural *C, *D, *N;
+        //берем абсолютные величины
+		C = Z1(A);
         D = Z1(B);
-        C = N8(C,D);
-        R = init_integer(C -> length);
-        for(size_t i = 0; i < (C -> length); i++) 
-            R -> digits[i] = (C -> digits[i]);
-        R -> sign = true;
-        if(((q == 1) && (f == 2)) || ((q == 2) && (f == 1))) R = Z3(R);
+        //перемножаем их
+		N = N8(C,D);
+        
+		R = init_integer(N -> length);
+        //записываем в результат
+		for(size_t i = 0; i < (N -> length); i++) 
+            R -> digits[i] = (N -> digits[i]);
+        
+		R -> sign = true;
+        
+		free_natural(C);
+		free_natural(D);
+		free_natural(N);
+		
+		if(((q == 1) && (f == 2)) || ((q == 2) && (f == 1))){
+			if(R -> sign == true) R -> sign = false;
+			else R -> sign = true;
+		}
     }
     else
     {
