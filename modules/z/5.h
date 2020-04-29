bf9758df2dda050c54607f3430faba22ed1b5c5e
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:40:59 +0300

    Update 5.h

--- modules/z/5.h
+++ modules/z/5.h
@@ -1,9 +1,11 @@
+//Когутенко Андрей 9305
+//Преобразование целого неотрицательного в натуральное
 natural *Z5(integer *A)
 {
     size_t i;
     natural *B;
     B = init_natural(A -> length);
     for(i = 0 ; i < B-> length ; i++) 
-        B -> digits[i] = A -> digits[i];
+        B -> digits[i] = A -> digits[i];//Копирование цифр
     return B;
 }
