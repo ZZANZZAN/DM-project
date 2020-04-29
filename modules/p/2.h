e8eeeac2d3e739ab84714d7c1d3589cda19ea278
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 11:01:28 +0300

    Update 2.h

--- modules/p/2.h
+++ modules/p/2.h
@@ -1,15 +1,19 @@
+//Салауров Евгений 9305
+//Вычитание полиномов
 polynomial *P2(polynomial *A, polynomial *B)// модуль вычитания двух многочленов
 {
   size_t i = 0;//переменная для прохождению по массиву
-    polynomial *C = copy_polynomial(A);
-  
+
+
   if(A -> degree > B -> degree){//определяем степень какого многочлена больше
+    polynomial *C = copy_polynomial(A);
     for(i = 0; i < B -> degree+1; i++){
       C -> factors[i] = Q6(A -> factors[i], B -> factors[i]);//вызов функции вычитания рациональных дробей
     }
   }
-  else{//все действия происходят аналогично но многочлены меняются местами 
+  else{//все действия происходят аналогично но многочлены меняются местами
     for(i = 0; i < A -> degree+1; i++){
+    polynomial *C = copy_polynomial(B);
       C -> factors[i] = Q6(B -> factors[i], A -> factors[i]);//вызов функции вычитания рациональных дробей
     }
   }
