507907651db4b35bc6441afe3cb61897933682f1
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:11:20 +0300

    Update 9.h

--- modules/n/9.h
+++ modules/n/9.h
@@ -1,8 +1,10 @@
+//Кацер Евгений 9307
+//Этот модуль находит разность между первым числом и вторым, умноженным на цифру
 natural *N9(natural *A, natural *B, int c)
 {
     natural *C = NULL, *res = NULL;
-    C = N6 (B , c);
-    res = N5 (A , C);
+    C = N6 (B , c);//Умножаем число на цифру
+    res = N5 (A , C);//Находим их разность
     free_natural(C);
     return res;
 }
