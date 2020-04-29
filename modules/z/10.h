350e0626c47382aba2c6f863f5701fdb5be50261
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 04:07:58 +0300

    Update 10.h

--- modules/z/10.h
+++ modules/z/10.h
@@ -1,9 +1,11 @@
+//Чугуй Владимир 9305
+//Остаток от деления целого числа на натуральное
 integer *Z10(integer *A, integer *B) {
     integer *Result, *quotient, *temp_result;
-    quotient = Z9(A, B);
-    Result = Z8(quotient, B);
+    quotient = Z9(A, B);//Вычисление частного
+    Result = Z8(quotient, B);//Умножение частного на делитель
     temp_result = Result;
-    Result = Z7(A, temp_result);
+    Result = Z7(A, temp_result);//Вычисление остатка
     free_integer(temp_result);
     free_integer(quotient);
 
