f1585cd11cf84fe92d0bd5e031cdc7208639cae0
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:27:50 +0300

    Update 13.h

--- modules/n/13.h
+++ modules/n/13.h
@@ -1,26 +1,28 @@
+//Ковалёва Ксения 9305
+//Нахождение НОД двух чисел
 natural *N13(natural *A,natural *B)
 {
     natural *first = NULL, *second = NULL, *temp = NULL;
     first = copy_natural(A);
     second = copy_natural(B);
-    while(N2(first) && N2(second))
+    while(N2(first) && N2(second))//Ниже реализован алгоритм Евклида через остатки от деления
     {
         if(N1(first , second)==2)
         {
-            temp = N12(first , second);
+            temp = N12(first , second);//Вычисление остатка от деления
             free_natural(first);
             first = NULL;
             first = temp;
         }
         else
         {
-            temp = N12(second , first);
+            temp = N12(second , first);//Вычисление остатка от деления
             free_natural(second);
             second = NULL;
             second = temp;
         }
     }
-    if(N2(first))
+    if(N2(first))//Проверка на ноль
     {
         free_natural(second);
         return first;
