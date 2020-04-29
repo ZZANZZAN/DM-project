647235f5b337918645eee288f6d5bc6f21804983
VolodyaZAVR <62401307+VolodyaZAVR@users.noreply.github.com>
2020-04-29 03:07:38 +0300

    Update 8.h

--- modules/n/8.h
+++ modules/n/8.h
@@ -1,3 +1,5 @@
+//Бессонов Никита 9305
+//Этот модуль умножает натуральные числа
 natural *N8(natural *A, natural *B)
 {
     natural *res;
@@ -9,14 +11,14 @@ natural *N8(natural *A, natural *B)
     if (A -> length > B -> length)
     {
         temp_length = B -> length;
-        for (i = 0 ; i < temp_length; i++)
+        for (i = 0 ; i < temp_length; i++)//Идём циклом до длины самого короткого числа
         {
-            temp = N6 (A, B -> digits[i]);
+            temp = N6 (A, B -> digits[i]);//Умножаем число на последнюю цифру второго числа
             t = temp;
-            temp = N7 (temp , i);
+            temp = N7 (temp , i);//Добавляем i нулей в конец числа
             free_natural(t);
             t = res;
-            res = N4 (temp , res);
+            res = N4 (temp , res);//Суммируем полученное число с результатом
             free_natural(t);
             free_natural(temp);
         }
@@ -24,14 +26,14 @@ natural *N8(natural *A, natural *B)
     else
     {
         temp_length = A -> length;
-        for (i = 0 ; i < temp_length ; i++)
+        for (i = 0 ; i < temp_length ; i++)//Идём циклом до длины самого короткого числа
         {
-        	temp = N6 (B , A -> digits[i]);
+        	temp = N6 (B , A -> digits[i]);//Умножаем число на последнюю цифру второго числа
             t = temp;
-            temp = N7 (temp , i);
+            temp = N7 (temp , i);//Добавляем i нулей в конец числа
             free_natural(t);
             t = res;
-            res = N4 (temp , res);
+            res = N4 (temp , res);//Суммируем полученное число с результатом
             free_natural(t);
             free_natural(temp);
         }
